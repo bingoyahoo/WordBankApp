0545590f34916752bd2afb940cb3bdb18a06aec5
delvinlow <delvinlowzy@gmail.com>
2016-05-04 16:18:49 +0800

    Display list of vocabulary

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ var app = angular.module("WordBank", []);
 
 app.controller("MainCtrl", ["$scope", function($scope){
     $scope.testVariable = 8;
+    $scope.terms = [
+        {"word": "camaraderie", "definition": "comradeship; good-fellowship." },
+        {"word": "venison", "definition": "the flesh of a deer or similar animal as used for food."},
+        {"word": "assuage", "definition": "to appease; satisfy; allay; relieve"},
+        {"word": "gnarled", "definition": "contorted, distorted"},
+        {"word": "imperative", "definition": "absolutely necessary"}
+    ];
+    $scope.orderProp = "word";
 }]);
