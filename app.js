34f28bd9a7c1793bcfac47341e9862aeb7aff757
delvinlow <delvinlowzy@gmail.com>
2016-05-04 15:55:10 +0800

    Set up initial Angularjs app

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * Created by delvinlow on 4/5/16.
+ */
+
+var app = angular.module("WordBank", []);
+
+app.controller("MainCtrl", ["$scope", function($scope){
+    $scope.testVariable = 8;
+}]);
