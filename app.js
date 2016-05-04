d0a5d7695c537a080dc0868bc2f95c424109ac56
delvinlow <delvinlowzy@gmail.com>
2016-05-04 18:10:35 +0800

    Added a service to store data

--- app.js
+++ app.js
@@ -5,18 +5,25 @@
 
 var app = angular.module("WordBank", []);
 
-app.controller("MainCtrl", ["$scope", function($scope){
+app.factory("words", [function(){
+    var obj = {
+        terms: [
+            {"word": "camaraderie", "definition": "comradeship; good-fellowship." },
+            {"word": "venison", "definition": "the flesh of a deer or similar animal as used for food."},
+            {"word": "assuage", "definition": "to appease; satisfy; allay; relieve"},
+            {"word": "gnarled", "definition": "contorted, distorted"},
+            {"word": "imperative", "definition": "absolutely necessary"}
+        ]
+    };
+    return obj;
+}]);
+
+app.controller("MainCtrl", ["$scope", "words", function($scope, words){
     $scope.testVariable = 8;
     $scope.newEntry = "";
     $scope.newDefinition = "";
 
-    $scope.terms = [
-        {"word": "camaraderie", "definition": "comradeship; good-fellowship." },
-        {"word": "venison", "definition": "the flesh of a deer or similar animal as used for food."},
-        {"word": "assuage", "definition": "to appease; satisfy; allay; relieve"},
-        {"word": "gnarled", "definition": "contorted, distorted"},
-        {"word": "imperative", "definition": "absolutely necessary"}
-    ];
+    $scope.terms = words.terms;
     $scope.orderProp = "word";
 
     $scope.addTerm = function(){
@@ -28,3 +35,4 @@ app.controller("MainCtrl", ["$scope", function($scope){
         $scope.newDefinition = "";
     };
 }]);
+
